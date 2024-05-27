5c037ad6471a83cd44379c8591d43ef13436b3bb
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:08:46 +0200

    Update ResourceNotFoundException.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/ResourceNotFoundException.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package com.enviro.assessment.grad001.SiyabongaHadebe.exception;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Annotates this class to return a 404 status code when thrown
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
