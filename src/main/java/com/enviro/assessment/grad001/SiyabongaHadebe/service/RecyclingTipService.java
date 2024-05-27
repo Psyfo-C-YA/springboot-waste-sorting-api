d4b1d51f950f201a8d03e50aef058a97138a292f
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 11:20:39 +0200

    Update RecyclingTipService.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/RecyclingTipService.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/RecyclingTipService.java
@@ -5,12 +5,22 @@ import com.enviro.assessment.grad001.SiyabongaHadebe.requests.RecyclingTipReques
 
 import java.util.List;
 
+// Interface defining the contract for recycling tip services
 public interface RecyclingTipService {
 
+    // Method to get all recycling tips
     List<RecyclingTipDTO> findAllRecyclingTips();
+
+    // Method to get a recycling tip by ID
     RecyclingTipDTO findRecyclingTipById(Long id);
+
+    // Method to create a new recycling tip
     void createRecyclingTip(RecyclingTipRequest recyclingTipRequest, Long categoryId);
+
+    // Method to update a recycling tip
     void updateRecyclingTip(Long id, RecyclingTipRequest recyclingTipRequest, Long categoryId);
+
+    // Method to delete a recycling tip by ID
     void deleteRecyclingTip(Long id);
 
 }
