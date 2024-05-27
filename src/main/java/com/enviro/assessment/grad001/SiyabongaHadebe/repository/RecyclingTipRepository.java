2806d82a92ef5cb93d7d5f284311794e1f66af13
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 10:47:53 +0200

    Update RecyclingTipRepository.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/repository/RecyclingTipRepository.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/repository/RecyclingTipRepository.java
@@ -2,9 +2,13 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.repository;
 
 import com.enviro.assessment.grad001.SiyabongaHadebe.model.RecyclingTip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface RecyclingTipRepository extends JpaRepository<RecyclingTip, Long> {
 
+    // Custom query method to check if a recycling tip exists by ID
     boolean existsRecyclingTipById(Long id);
 
 }
