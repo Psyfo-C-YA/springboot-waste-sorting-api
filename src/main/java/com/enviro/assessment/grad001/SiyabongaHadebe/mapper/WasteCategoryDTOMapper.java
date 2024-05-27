520d146e46d01d07b232d54059a6b45317c30d6c
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:18:33 +0200

    Update WasteCategoryDTOMapper.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/WasteCategoryDTOMapper.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/WasteCategoryDTOMapper.java
@@ -14,24 +14,30 @@ import java.util.stream.Collectors;
 @Service
 public class WasteCategoryDTOMapper implements Function<WasteCategory, WasteCategoryDTO> {
 
+    // Injects disposal guideline data transfer object mapper dependency
     @Autowired
     private DisposalGuidelineDTOMapper disposalGuidelineDTOMapper;
 
+    
+    // Injects recycling tip data transfer object mapper dependency
     @Autowired
     private RecyclingTipDTOMapper recyclingTipDTOMapper;
 
     @Override
     public WasteCategoryDTO apply(WasteCategory wasteCategory) {
+        // Convert the list of DisposalGuidelines to DisposalGuidelineDTOs
         List<DisposalGuidelineDTO> guidelines = wasteCategory.getDisposalGuidelines()
                 .stream()
                 .map(disposalGuidelineDTOMapper)
                 .collect(Collectors.toList());
 
+        // Convert the list of RecyclingTips to RecyclingTipDTOs
         List<RecyclingTipDTO> tips = wasteCategory.getRecyclingTips()
                 .stream()
                 .map(recyclingTipDTOMapper)
                 .collect(Collectors.toList());
 
+        // Return a new WasteCategoryDTO with the mapped fields
         return new WasteCategoryDTO(
                 wasteCategory.getId(),
                 wasteCategory.getName(),
