54ae5e65a090e6b9b6c595832fc0a29d6936837e
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:27:05 +0200

    Update RecyclingTipDTOMapper.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/RecyclingTipDTOMapper.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/RecyclingTipDTOMapper.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
+/**
+ * Service class to map RecyclingTip entities to RecyclingTipDTOs.
+ * This class is annotated as a Spring service, indicating that it is a candidate
+ * for Spring's dependency injection mechanism and can be autowired into other components.
+ */
 @Service
 public class RecyclingTipDTOMapper implements Function<RecyclingTip, RecyclingTipDTO> {
 
     @Override
     public RecyclingTipDTO apply(RecyclingTip recyclingTip) {
+        // Creating and returning a new RecyclingTipDTO object with values from the RecyclingTip entity
         return new RecyclingTipDTO(
                 recyclingTip.getId(),
                 recyclingTip.getTip()
