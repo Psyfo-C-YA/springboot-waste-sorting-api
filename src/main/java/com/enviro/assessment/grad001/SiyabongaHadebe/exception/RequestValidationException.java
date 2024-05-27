3ab537b42aa09576b5fc2f9439e5b3965b01a40d
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:07:28 +0200

    Update RequestValidationException.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/RequestValidationException.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/RequestValidationException.java
@@ -3,6 +3,7 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Annotates this class to return a 400 status code when thrown
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class RequestValidationException extends RuntimeException {
     public RequestValidationException(String message) {
