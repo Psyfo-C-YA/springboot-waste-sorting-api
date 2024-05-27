e8bc2b90fc8538b6eecfbd9af8f20b6cb4aed2aa
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 10:47:06 +0200

    Update DisposalGuidelineRepository.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/repository/DisposalGuidelineRepository.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/repository/DisposalGuidelineRepository.java
@@ -2,7 +2,12 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.repository;
 
 import com.enviro.assessment.grad001.SiyabongaHadebe.model.DisposalGuideline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Long> {
+
+    // Custom query method to check if a disposal guideline exists by ID
     boolean existsDisposalGuidelineById(Long id);
+    
 }
