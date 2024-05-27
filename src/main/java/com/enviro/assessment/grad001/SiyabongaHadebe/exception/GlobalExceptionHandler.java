c54e933218e3ae6fcb790e1ccf7b72cfd53e086a
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:04:02 +0200

    Update GlobalExceptionHandler.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/GlobalExceptionHandler.java
@@ -5,12 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+// Annotates this class as a global exception handler
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Handles ResourceNotFoundException
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Handles DuplicateResourceException
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<String> handleDuplicateResourceException(DuplicateResourceException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    // Handles RequestValidationException
+    @ExceptionHandler(RequestValidationException.class)
+    public ResponseEntity<String> handleRequestValidationException(RequestValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
