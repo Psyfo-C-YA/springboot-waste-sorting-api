abb51ca048694d37ff7d47f69fbebaf3b8af1351
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:07:58 +0200

    Update DuplicateResourceException.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/DuplicateResourceException.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/exception/DuplicateResourceException.java
@@ -3,6 +3,7 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Annotates this class to return a 409 status code when thrown
 @ResponseStatus(code = HttpStatus.CONFLICT)
 public class DuplicateResourceException extends RuntimeException {
     public DuplicateResourceException(String message) {
