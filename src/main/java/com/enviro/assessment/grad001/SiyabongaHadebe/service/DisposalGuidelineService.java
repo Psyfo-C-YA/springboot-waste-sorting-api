9923cb40a8311319acfa767f60166619320ab941
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 11:01:31 +0200

    Update DisposalGuidelineService.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/DisposalGuidelineService.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/DisposalGuidelineService.java
@@ -5,12 +5,22 @@ import com.enviro.assessment.grad001.SiyabongaHadebe.requests.DisposalGuidelineR
 
 import java.util.List;
 
+// Interface defining the methods for disposal guideline services
 public interface DisposalGuidelineService {
 
+    // Method to get all disposal guidelines
     List<DisposalGuidelineDTO> findAllDisposalGuidelines();
+
+    // Method to get a disposal guideline by ID
     DisposalGuidelineDTO findDisposalGuidelineById(Long id);
+
+     // Method to delete a disposal guideline by ID
     void deleteDisposalGuideline(Long id);
+
+    // Method to create a new disposal guideline
     void createDisposalGuideline(DisposalGuidelineRequest guideLineRequest, Long categoryId);
+
+    // Method to update a disposal guideline
     void updateDisposalGuideline(Long id, DisposalGuidelineRequest guideLineRequest, Long categoryId);
 
 }
