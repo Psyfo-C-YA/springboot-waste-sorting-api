822055078169b86541f25facd44ca8a4b5968d5c
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 11:59:08 +0200

    Update RecyclingTipController.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/controller/RecyclingTipController.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/controller/RecyclingTipController.java
@@ -19,34 +19,23 @@ import java.util.List;
 @Validated
 public class RecyclingTipController {
 
+    // Injects the RecyclingTipService dependency
     @Autowired
     private RecyclingTipService recyclingTipService;
 
-    /**
-     * Retrieves all recycling tips.
-     * @return a list of recycling tips.
-     */
+    // Handles GET requests to retrieve all recycling tips
     @GetMapping
     public List<RecyclingTipDTO> getAllRecyclingTips() {
         return recyclingTipService.findAllRecyclingTips();
     }
 
-    /**
-     * Retrieves a recycling tip by its ID.
-     * @param id the ID of the recycling tip.
-     * @return the recycling tip, or 404 if not found.
-     */
+    // Handles GET requests to retrieve a recycling tip by ID
     @GetMapping("/{id}")
     public ResponseEntity<RecyclingTipDTO> getRecyclingTipById(@PathVariable Long id) {
         return ResponseEntity.ok(recyclingTipService.findRecyclingTipById(id));
     }
 
-    /**
-     * Creates a new recycling tip.
-     * @param recyclingTipRequest the recycling tip to create.
-     * @param wasteCategoryId the recycling tip to create.
-     * @return the created recycling tip.
-     */
+    // Handles POST requests to create a new recycling tip
     @PostMapping("{wasteCategoryId}")
     public void createRecyclingTip(
             @RequestBody @Validated RecyclingTipRequest recyclingTipRequest ,
@@ -54,6 +43,7 @@ public class RecyclingTipController {
         recyclingTipService.createRecyclingTip(recyclingTipRequest, wasteCategoryId);
     }
 
+    // Handles PUT requests to update an existing recycling tip
     @PutMapping("{id}/tip/{wasteCategoryId}")
     public void updateRecyclingTip(@PathVariable("id") Long id,
                                                     @RequestBody @Validated RecyclingTipRequest recyclingTipRequest,
@@ -61,11 +51,7 @@ public class RecyclingTipController {
         recyclingTipService.updateRecyclingTip(id,recyclingTipRequest,wasteCategoryId);
     }
 
-    /**
-     * Deletes a recycling tip by its ID.
-     * @param id the ID of the recycling tip to delete.
-     * @return 204 No Content if successful, 404 if not found.
-     */
+    // Handles DELETE requests to delete a recycling tip by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteRecyclingTip(@PathVariable Long id) {
         recyclingTipService.deleteRecyclingTip(id);
