e8ca9798225f87c53d7995d5c895cb11f7977987
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:30:35 +0200

    Update WasteCategoryRequest.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/requests/WasteCategoryRequest.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/requests/WasteCategoryRequest.java
@@ -3,5 +3,6 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.requests;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.lang.NonNull;
 
+// Request class for creating or updating a category
 public record WasteCategoryRequest(@NonNull @NotBlank String name) {
 }
