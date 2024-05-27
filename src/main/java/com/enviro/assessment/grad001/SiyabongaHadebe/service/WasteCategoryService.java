ddcbabb22b640d0828fd3f750ea9289438b846f6
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 10:51:02 +0200

    Update WasteCategoryService.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/WasteCategoryService.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/service/WasteCategoryService.java
@@ -5,11 +5,24 @@ import com.enviro.assessment.grad001.SiyabongaHadebe.requests.WasteCategoryReque
 
 import java.util.List;
 
+// Interface defining the methods for category services
 public interface WasteCategoryService {
+
+    // Method to get all categories
     List<WasteCategoryDTO> findAllWasteCategories();
+
+    // Method to get a category by ID
     WasteCategoryDTO findWasteCategoryById(Long id);
+
+    // Method to get a category by name
     WasteCategoryDTO findWasteCategoryByName(String name);
+
+     // Method to create a new category
     void createWasteCategory(WasteCategoryRequest wasteCategoryRequest);
+
+    // Method to delete a category by ID
     void deleteWasteCategory(Long id);
+
+    // Method to update a category
     void updateWasteCategory(Long id, WasteCategoryRequest wasteCategoryRequest);
 }
