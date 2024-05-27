d122d2a26b10fcb9bdab3bc90f0bd079b202b3df
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:28:13 +0200

    Update WasteCategoryDTO.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/dto/WasteCategoryDTO.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/dto/WasteCategoryDTO.java
@@ -2,5 +2,6 @@ package com.enviro.assessment.grad001.SiyabongaHadebe.dto;
 
 import java.util.List;
 
+// Data Transfer Object for WasteCategory
 public record WasteCategoryDTO(Long id, String name, List<DisposalGuidelineDTO> disposalGuidelines, List<RecyclingTipDTO> tips) {
 }
