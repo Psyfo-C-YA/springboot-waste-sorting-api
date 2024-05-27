a579b7ab1455aafea8a98ad6fa0ebfc82cc8778c
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-05-27 12:23:33 +0200

    Update DisposalGuidelineDTOMapper.java

--- src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/DisposalGuidelineDTOMapper.java
+++ src/main/java/com/enviro/assessment/grad001/SiyabongaHadebe/mapper/DisposalGuidelineDTOMapper.java
@@ -9,8 +9,10 @@ import java.util.function.Function;
 @Service
 public class DisposalGuidelineDTOMapper implements Function<DisposalGuideline, DisposalGuidelineDTO> {
 
+    // The apply method is overridden to define the mapping from DisposalGuideline to DisposalGuidelineDTO
     @Override
     public DisposalGuidelineDTO apply(DisposalGuideline disposalGuideline) {
+        // Creating and returning a new DisposalGuidelineDTO object with values from the DisposalGuideline entity
         return new DisposalGuidelineDTO(
                 disposalGuideline.getId(),
                 disposalGuideline.getDescription()
